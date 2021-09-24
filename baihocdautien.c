5f7bd480ed82d045c988000f7c9e69564bfe682b
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2021-09-24 21:05:10 +0700

    Add files via upload

---
+++ baihocdautien.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {	
+	printf("Than em vua trang lai vua tron\n");
+	printf("Ba chim bay noi voi nuoc non\n");	
+	printf("Ran nat mac dau tay ke lan\n");	
+	printf("Ma em van giu tam long son\n");		
+	return 0;
+}
